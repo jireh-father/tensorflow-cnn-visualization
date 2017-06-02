772b6162abe22f42646c539e07022f1da8a3d95e
서일근 <seoilgun@gmail.com>
2017-06-02 14:39:25 +0900

    commit

--- visualizer.py
+++ visualizer.py
@@ -5,7 +5,7 @@ def convert_weights(weights):
     x_min = tf.reduce_min(weights)
     x_max = tf.reduce_max(weights)
     weights_0_to_1 = (weights - x_min) / (x_max - x_min)
-    # weights_0_to_255_uint8 = tf.image.convert_image_dtype(weights_0_to_1, dtype=tf.uint8)
+    weights_0_to_255_uint8 = tf.image.convert_image_dtype(weights_0_to_1, dtype=tf.uint8)
 
     return tf.transpose(weights_0_to_1, [3, 0, 1, 2])
 
@@ -18,13 +18,13 @@ def summary_filter(weights, filter_summary_limit=3):
 def summary_filters(weights_list, batch_limit=3, feature_map_limit=3):
     for weight_idx, weights in enumerate(weights_list):
         weights_transposed = convert_weights(weights)
-        summary_name = "filter_visualization/layer%d" % weight_idx
+        summary_name = "filter_visualization_layer%d" % weight_idx
         for i in range(weights_transposed.shape[0]):
             if weights_transposed.shape[3] == 3:
-                tf.summary.image("%s_%d" % (summary_name, i), weights_transposed[i:i + 1, :, :, :])
+                tf.summary.image("%s/%d" % (summary_name, i), weights_transposed[i:i + 1, :, :, :])
             else:
                 for j in range(weights_transposed.shape[3]):
-                    tf.summary.image("%s_%d/filter%d" % (summary_name, i, j),
+                    tf.summary.image("%s/%d/filter%d" % (summary_name, i, j),
                                      weights_transposed[i:i + 1, :, :, j:j + 1])
                     if j + 1 >= feature_map_limit:
                         break
