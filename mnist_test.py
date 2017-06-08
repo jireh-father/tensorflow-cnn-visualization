b95d861e548635266958c53af84b19954eabe133
서일근 <seoilgun@gmail.com>
2017-06-08 13:22:50 +0900

    commit

--- mnist_test.py
+++ mnist_test.py
@@ -4,6 +4,7 @@ import os
 import mnist_loader as loader
 import visualizer
 import time
+import summarizer
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -92,3 +93,7 @@ with tf.Session() as sess:
     summary = sess.run(merged, feed_dict={inputs: validation_sample})
     writer.add_summary(summary, 0)
     writer.close()
+
+    summarizer.summary_embedding_with_labels(sess, validation_data[0:batch_size], validation_labels[0:batch_size],
+                                             [relu, logits], inputs, FLAGS.summary_path, image_size, channel=1,
+                                             batch_size=batch_size)
